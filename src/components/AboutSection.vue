e8da46139e193fbe93ee4a5b41e35cf987af8958
raghavraj-27 <raghvendraprasad5678@gmail.com>
2023-02-10 17:14:15 +0530

    all done

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,63 @@
+<template>
+  <section id="about">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="7">
+              <!-- <h1 class="font-weight-light display-2">Sobre</h1> -->
+              <h1 class="font-weight-light display-1 mb-3">
+                All in one institute management app
+              </h1>
+              <v-row>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon2.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    Skip that endless process of maintaining registers for entries.
+                  </p>
+                </v-col>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon1.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    No need for hiring developers for building/updating websites to manage enquiries.
+                  </p>
+                </v-col>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon3.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    With Aintrie all admin needs to do is feed in the information about the institution and get a professional website. Track and follow-up on all enquiries in different stages until closure. Make data driven institutes about your institute with the insights you get through our app.
+                  </p>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="5" class="d-none d-md-flex">
+              <v-img
+                src="@/assets/img/ill1.svg"
+                class="d-block ml-auto mr-auto"
+                max-width="400px"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<style scoped>
+#about {
+  background-color: #f4f7f5;
+}
+</style>
---
+++ src/components/ContactSection.vue
@@ -0,0 +1,155 @@
+<template>
+  <section class="pb-8" id="contact">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row justify="center">
+            <v-col cols="12" sm="5">
+              <h1 class="font-weight-light display-1 green--text">For Any Assistance Required Please Reach Out</h1>
+              <!-- <h3 class="font-weight-light mt-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+                explicabo commodi quisquam asperiores dolore ad enim provident
+                veniam perferendis voluptate, perspiciatis.
+              </h3> -->
+              <!-- <h3 class="font-weight-light mt-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+              </h3> -->
+              <h3 class="font-weight-light mt-3 green--text">
+                Telefone: +91 96932 43217
+              </h3>
+              <h3 class="font-weight-light green--text">
+                Email: info@codingage.biz
+              </h3>
+            </v-col>
+            <v-col cols="12" sm="7">
+              <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+                <v-text-field
+                    v-model="name"
+                    color="black"
+                    :rules="nameRules"
+                    label="Name"
+                    required
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="email"
+                    color="black"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                ></v-text-field>
+
+                <v-textarea
+                    v-model="textArea"
+                    color="black"
+                    :rules="textAreaRules"
+                    label="Leave us a message"
+                    required
+                />
+
+                <v-btn
+                    :disabled="!valid"
+                    color="green"
+                    :dark="valid"
+                    rounded
+                    block
+                    class="mt-3"
+                    @click="submit"
+                >
+                  Submit
+                </v-btn>
+              </v-form>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="svg-border-waves text-white">
+      <v-img src="~@/assets/img/borderWavesBlue.svg"/>
+    </div>
+    <v-snackbar
+        v-model="snackbar.enabled"
+        timeout="3000"
+        right
+        top
+        :color="snackbar.color"
+    >
+      {{ snackbar.text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            text
+            v-bind="attrs"
+            @click="snackbar.enabled = false"
+        >
+          Fechar
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </section>
+</template>
+
+<style scoped>
+#contact {
+  background-color: #f4f7f5;
+}
+
+.svg-border-waves .v-image {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 3rem;
+  width: 100%;
+  overflow: hidden;
+}
+
+</style>
+
+<script>
+// import {db} from '@/main'
+
+export default {
+  data: () => ({
+    icons: ["fa-facebook", "fa-twitter", "fa-linkedin", "fa-instagram"],
+    valid: true,
+    name: "",
+    nameRules: [
+      (v) => !!v || "The name field is required",
+      (v) => (v && v.length >= 6) || "Name must be more than 6 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "The email field is mandatory",
+      (v) => /.+@.+\..+/.test(v) || "Not a valid email address",
+    ],
+    textArea: "",
+    textAreaRules: [
+      (v) => !!v || "The text field is required",
+      (v) => (v && v.length >= 10) || "Minimum of 10 characters",
+    ],
+    lazy: false,
+    snackbar: {
+      enabled: false,
+      text: '',
+      color: ''
+    }
+  }),
+  methods: {
+    submit() {
+      /*db.collection("contactData").add({
+        name: this.name,
+        email: this.email,
+        message: this.textArea
+      }).then(() => {
+        this.snackbar.text = "Mensagem enviada com sucesso"
+        this.snackbar.color = "success"
+        this.snackbar.enabled = true
+      }).catch(() => {
+        this.snackbar.text = "Erro ao enviar mensagem"
+        this.snackbar.color = "danger"
+        this.snackbar.enabled = true
+      })*/
+    }
+  }
+};
+</script>
